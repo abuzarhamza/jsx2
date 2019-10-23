05be1e2954001bb8b715ec55e08a214976bb9c32
abuzarhamza <abuzarhamza@gmail.com>
2019-10-23 18:21:16 -0500

    comiited the problem

---
+++ src/.history/index_20191023181609.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    //FIX THE ERROR IN THE JSX FORMAT
+    return (
+        <div>
+            <label class="label" for="name">
+                Enter name: 
+            </label>
+            <input id="name" type="text">
+            <button style="background-color: blue;color: white; ">Submit</button>
+        </div>
+    );
+};
+
+ReactDOM.render( <App />, document.querySelector('#root'));
