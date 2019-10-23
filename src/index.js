04a91f08b5708cbe4ce1bd72faa427357c3b9840
abuzarhamza <abuzarhamza@gmail.com>
2019-10-23 18:35:33 -0500

    solution

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ const App = () => {
             <label class="label" for="name">
                 Enter name: 
             </label>
-            <input id="name" type="text">
-            <button style="background-color: blue;color: white; ">Submit</button>
+            <input id="name" type="text"/>
+            <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
         </div>
     );
 };
